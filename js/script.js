156c7970a1016aee966e2fa95af0926f0f15e425
jrichardlai <jrichardlai@gmail.com>
2011-02-18 23:07:01 -0800

    Edited layout removed log.

--- js/script.js
+++ js/script.js
@@ -50,7 +50,6 @@ LocationRow = $.klass({
     this.time = time_difference + (3600000 * (this.offset - current_offset)) + current_time;
     var text_date = formatDate(new Date(this.time), date_format)
     $('.time input', this.element).val(text_date).data('old-value', text_date);
-    log(this.info_window_content);
     $('.time', this.info_window_content).html(text_date);
   },
   setName: function(name){
@@ -135,7 +134,6 @@ $(document).ready(function(){
     //TODO Verify that the date is valid
 
     time_difference = time_difference + getDateFromFormat($(this).val(), date_format) - getDateFromFormat($(this).data('old-value'), date_format);
-    log({time_difference: time_difference, 'real-time': $(this).attr('real-time'), current_time: current_time, val: $(this).val()});
     $.each(locationsArray, function(index, element){
       element.updateTime();
     })
@@ -158,7 +156,6 @@ $(document).ready(function(){
 });
 
 function loadLocations(cookie_locations_array) {
-  log(cookie_locations_array);
   $.each(cookie_locations_array, function(index, element) {
     if (element.lat && element.lng) {
       var location_point  = new google.maps.LatLng(element.lat, element.lng);
